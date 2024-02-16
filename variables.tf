c804b8a7651347db28b05921207c248e0ec09669
Pawel Farys <pmg.farys@gmail.com>
2024-02-16 23:20:50 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
 variable "name" {
   type    = string
-  default = ""
-}
-
-variable "env" {
-  type = string
 }
 
 variable "cidr" {
+  type        = string
   default     = "172.10.0.0/16"
-  description = "CIDR to use for VPC and its subnets. In /16 mask."
+  description = "CIDR to use for VPC and its subnets."
 }
 
 variable "subnets" {
+  type    = list(string)
   default = ["a", "b", "c"]
 }
 
 variable "enable_dns_hostnames" {
+  type    = bool
   default = true
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "A mapping of resource tags."
 }
